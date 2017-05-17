34a13274b517127f33a5195bdb5413698e29090e
Виталия С. Перец <08672@zp.itstep.org>
2017-05-17 18:49:24 +0300

    add constructor with parametres

---
+++ consoleFraction/Fraction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleFraction
+{
+    class Fraction
+    {
+        private int _den;
+        public int Num { get; set; }
+
+        public int Den { get { return _den; } set { _den = value; } }
+        public Fraction()
+        {
+            this.Num = 0;
+            _den = 1;
+        }
+
+        public override string ToString()
+        {
+            return $"{Num} / {_den}";
+        }
+
+        public Fraction(int n, int d)
+        {
+            this.Num = n;
+            this._den = d;
+        }
+    }
+}
